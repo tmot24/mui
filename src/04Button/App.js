f8f4561895cd2d914dd9cb100bb55267ca0ee4e5
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-04-26 15:49:10 +0300

    #4: Button

---
+++ src/04Button/App.js
@@ -0,0 +1,24 @@
+import {makeStyles} from "@material-ui/styles";
+import {theme} from "../theme";
+import {Button} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        color: "red",
+        border: "none",
+    }
+}));
+
+export const App = () => {
+    const classes = useStyles();
+
+    return (
+        <Button
+            className={classes.root}
+            variant={"outlined"}
+            color={"primary"}
+            size={"small"}
+        >
+            Primary</Button>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './02Second/components/App';
+import {App} from './04Button/App';
 import { ThemeProvider } from '@material-ui/core/styles';
 import {theme} from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
       <ThemeProvider theme={theme}>
-          <App/>
+          <div style={{
+              display: "flex",
+              justifyContent: "center",
+              }}>
+              <App/>
+          </div>
       </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
