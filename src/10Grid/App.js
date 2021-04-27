ea2ee2a49ad3c5981a65fa4316668b025ec8d60d
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-04-27 23:15:47 +0300

    #10: Grid & Card

--- src/10Grid/App.js
+++ src/10Grid/App.js
@@ -1,16 +1,25 @@
-import {Button, Grid, Paper} from "@material-ui/core";
+import {Grid, Paper} from "@material-ui/core";
+import {CardComponent} from "./Card";
 
 
 export const App = () => {
     return (
         <Paper square variant={"elevation"} style={{backgroundColor: "grey"}}>
-            <Grid container style={{justifyContent: "space-around", alignContent: "center", height: "100vh"}}>
-                <Grid item>
-                    <Button variant={"contained"}>Home</Button>
+            <Grid container
+                  style={{justifyContent: "center", alignContent: "center", height: "100vh"}}>
+                <Grid item style={{margin: "16px"}}>
+                    <CardComponent/>
                 </Grid>
-                <Grid item>
-                    <Button variant={"contained"}>Log in</Button>
+                <Grid item style={{margin: "16px"}}>
+                    <CardComponent/>
                 </Grid>
+                <Grid item style={{margin: "16px"}}>
+                    <CardComponent/>
+                </Grid>
+                <Grid item style={{margin: "16px"}}>
+                    <CardComponent/>
+                </Grid>
+
             </Grid>
         </Paper>
     );
