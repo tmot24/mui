8820ab402c544f438947a5cffe55f00749bfc18a
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-05-01 16:19:10 +0300

    #25: Accordion

---
+++ src/25Accordion/App.js
@@ -0,0 +1,73 @@
+import {Accordion, AccordionDetails, AccordionSummary} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Typography from "@material-ui/core/Typography";
+import {useState} from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const data = [
+    {
+        id: "panel1",
+        heading: "Panel 1",
+        secondaryHeading: "this is panel 1",
+        details: "Nulla facilisi. Phasellus sollicitudin nulla et"
+    },
+    {
+        id: "panel2",
+        heading: "Panel 2",
+        secondaryHeading: "this is panel 2",
+        details: " et et et et et "
+    }
+];
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: "100%"
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        flexBasis: "33.33%",
+        flexShrink: 0
+    },
+    secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary
+    }
+}));
+
+export const App = () => {
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
+
+    const handleChange = panel => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
+    return (
+        <div className={classes.root}>
+            {data.map(accordion => {
+                const { id, heading, secondaryHeading, details } = accordion;
+                return (
+                    <Accordion
+                        expanded={expanded === id}
+                        key={id}
+                        onChange={handleChange(id)}
+                    >
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1bh-content"
+                            id="panel1bh-header"
+                        >
+                            <Typography className={classes.heading}>{heading}</Typography>
+                            <Typography className={classes.secondaryHeading}>
+                                {secondaryHeading}
+                            </Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>{details}</Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                );
+            })}
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './24Snackbars/App';
+import {App} from './25Accordion/App';
 import {BrowserRouter} from "react-router-dom";
 import {CssBaseline} from "@material-ui/core";
 
